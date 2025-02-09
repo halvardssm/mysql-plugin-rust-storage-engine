cd8a91ee5ae3d40bcb0a921758f321a39661d9dc
Halvard Mørstad <halvard@simplyundoable.com>
2025-02-10 00:30:47 +0100

    removed old structure

--- crustdb/target/debug/deps/crustdb-3ee0dd4dd00afa64.d
+++
@@ -1,5 +0,0 @@
-/Users/halvardm/dev/mysql-rust-storage-engine/storage/crustdb/target/debug/deps/libcrustdb-3ee0dd4dd00afa64.rmeta: src/lib.rs
-
-/Users/halvardm/dev/mysql-rust-storage-engine/storage/crustdb/target/debug/deps/crustdb-3ee0dd4dd00afa64.d: src/lib.rs
-
-src/lib.rs:
--- crustdb/target/debug/deps/crustdb-5b999d73e588a3cb.d
+++
@@ -1,5 +0,0 @@
-/Users/halvardm/dev/mysql-rust-storage-engine/storage/crustdb/target/debug/deps/libcrustdb-5b999d73e588a3cb.rmeta: src/lib.rs
-
-/Users/halvardm/dev/mysql-rust-storage-engine/storage/crustdb/target/debug/deps/crustdb-5b999d73e588a3cb.d: src/lib.rs
-
-src/lib.rs:
