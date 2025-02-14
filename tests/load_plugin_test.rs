ff67616dc3669205680fbed21f7db04ade33ecf1
Halvard Mørstad <halvard@simplyundoable.com>
2025-02-14 01:45:34 +0100

    fmt

--- tests/load_plugin_test.rs
+++ tests/load_plugin_test.rs
@@ -1,7 +1,7 @@
-use std::error::Error;
-use podman_api::conn::TtyChunk;
-use tokio;
 use futures_util::stream::StreamExt;
+use podman_api::conn::TtyChunk;
+use std::error::Error;
+use tokio;
 
 use common::{container_exec, SetupTest};
 
@@ -10,9 +10,18 @@ mod common;
 #[tokio::test]
 async fn test_mariadb_query() -> Result<(), Box<dyn Error>> {
     let setup = SetupTest::init("1").await;
-    
+
     // let exec = container_exec(&container, vec!["mariadb", "--help"]).await?;
-    let exec = container_exec(&setup.container, vec!["mariadb", "--verbose","-e", "INSTALL PLUGIN rust_storage SONAME 'crustdb.so';"]).await?;
+    let exec = container_exec(
+        &setup.container,
+        vec![
+            "mariadb",
+            "--verbose",
+            "-e",
+            "INSTALL PLUGIN rust_storage SONAME 'crustdb.so';",
+        ],
+    )
+    .await?;
 
     let opts = Default::default();
     let mut exec_stream = exec.start(&opts).await.unwrap().unwrap();
